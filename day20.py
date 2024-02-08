55f9bed7c006777b3ba058136c9021e64993ad2b
minson0514 <minson0514@gmail.com>
2024-02-08 13:04:59 +0900

    v0.5 교수님꺼

--- day20.py
+++ day20.py
@@ -1,8 +1,9 @@
 import mymath
+import time
 
-if __name__ == "__main__": #모듈 배울 때 했다네요~
+if __name__ == "__main__":
     n = int(input("Input n : "))
     r = int(input("Input r : "))
-    print(f"{n}C{r} = {mymath.nCr(n,r)}")
-    #f = int(input())
-    #print(mymath.factorial(f))
+    print(f"{n}C{r} = {mymath.nCr(n, r)}")
+    # f = int(input())
+    # print(mymath.factorial(f))
--- mymath.py
+++ mymath.py
@@ -1,46 +1,34 @@
-"""
-#래지스터에있는 alu..?를 사용함
-def factorial(number)-> int: #-> 이 뒤에 오는게 리턴 타입
-    '''
-    factorial by repetition
-    Args:
-        number: number(int)
+import time
 
-    Returns: factorial result(int)
 
-    '''
-    result = 1
-    for i in range(1, number+1):
-        result = result * i
-    return result
-"""
+def timer(func):
+    return wrapper
 
-def factorial(number) -> int: #재귀함수, 스택메모리 사용
-    '''
-    factorial by recursion
-    Args:
-        number: number(int)
 
-    Returns:factorial result(int)
+# def factorial(number) -> int:
+#     '''
+#     factorial by repetition
+#     :param number: number (int)
+#     :return: factorial result (int)
+#     '''
+#     result = 1
+#     for i in range(1, number+1):
+#         result = result * i
+#     return result
 
-    '''
-    if number ==1:
-        return 1
-    else:
+
+def factorial(number) -> int:
         return number * factorial(number - 1)
 
 
-def nCr(n,r)-> int:
+@timer
+def nCr(n, r) -> int:  # SRP, OCP
     '''
-    조합 함수
-    Args:
-        n:
-        r:
-
-    Returns:
-
+    combination function
+    :param n:
+    :param r:
+    :return:
     '''
-    numerator = factorial(n) #분자
-    denominator = factorial(n-r) * factorial(r) #분모
-    return int(numerator / denominator) #강제 형변환, 변환 안 하면 double로 리턴되더라
-
+    numerator = factorial(n)
+    denominator = factorial(n-r) * factorial(r)
+    return int(numerator / denominator)
