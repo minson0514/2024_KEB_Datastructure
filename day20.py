c77e77658b16c1e2cbda050c3a4498340f0f3329
minson0514 <minson0514@gmail.com>
2024-02-08 10:27:51 +0900

    v0.2 combination

--- day20.py
+++ day20.py
@@ -16,7 +16,7 @@ def nCr(n,r)-> int:
     '''
     numerator = factorial(n) #분자
     denominator = factorial(n-r) * factorial(r) #분모
-    return int(numerator / denominator) #강제 형변환, 변환 안 하면 double로 리턴되더라..
+    return int(numerator / denominator) #강제 형변환, 변환 안 하면 double로 리턴되더라
 
 if __name__ == "__main__": #모듈 배울 때 했다네요~
     n = int(input("Input n : "))
