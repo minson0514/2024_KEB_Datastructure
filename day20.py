985500f2ca642910c6cff3b3e677d6bc618397ee
minson0514 <minson0514@gmail.com>
2024-02-08 10:41:31 +0900

    v0.3 recursion preview

--- day20.py
+++ day20.py
@@ -1,8 +1,32 @@
+"""
 def factorial(number)-> int: #-> 이 뒤에 오는게 리턴 타입
+    '''
+    factorial by repetition
+    Args:
+        number: number(int)
+
+    Returns: factorial result(int)
+
+    '''
     result = 1
     for i in range(1, number+1):
         result = result * i
     return result
+"""
+
+def factorial(number) -> int:
+    '''
+    factorial by recursion
+    Args:
+        number: number(int)
+
+    Returns:factorial result(int)
+
+    '''
+    if number ==1:
+        return 1
+    else:
+        return number * factorial(number - 1)
 
 def nCr(n,r)-> int:
     '''
@@ -19,6 +43,10 @@ def nCr(n,r)-> int:
     return int(numerator / denominator) #강제 형변환, 변환 안 하면 double로 리턴되더라
 
 if __name__ == "__main__": #모듈 배울 때 했다네요~
+    """
     n = int(input("Input n : "))
     r = int(input("Input r : "))
     print(f"{n}C{r} = {nCr(n,r)}")
+    """
+    f = int(input())
+    print(factorial(f))
